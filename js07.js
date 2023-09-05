432d60df4128131e12b10b1265115c691eec883e
emsseme <bk75121@gmail.com>
2023-09-05 16:56:15 +0900

    백팁 분리

--- js07.js
+++ js07.js
@@ -2,10 +2,10 @@ let person = 'Kim'
 let age = 25
 //console.log(`당신의 이름은 ${perosn}이고, 나이는 ${age}입니다.`)
 
-let tag = function(Strings, personExp, ageExp) {
-    console.log(Strings)
-    console.log(personExp)
-    console.log(ageExp)
+let tag = function(strings, personExp, ageExp) {
+    console.log(strings) //[ '그 사람 ', '은 ', '세 입니다. ' ]
+    console.log(personExp) //Kim
+    console.log(ageExp) //25
 }
-
+//문장과 데이터를 분리할 경우 활용
 let output = tag`그 사람 ${person}은 ${age}세 입니다. `
