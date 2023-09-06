4351d90068a75cee37b01fcfdedaa24e2ec001a7
emsseme <bk75121@gmail.com>
2023-09-06 15:08:28 +0900

    여러줄을 나타내는 템플릿 & 템플릿의 중첩 사용

--- js09.js
+++ js09.js
@@ -1,3 +1,8 @@
 console.log("1. 첫번째 줄\n두번째 줄")
+//여러줄을 나타내는 템플릿
 console.log(`2. 첫번째 줄
 두번째 줄`)
+//템플릿의 중첩 사용
+const class1 = `header ${ isLargeScreen() ? '' : 
+`icon-${item.isCollapsed} ? 'expander' : 
+'collapser'}`}`
