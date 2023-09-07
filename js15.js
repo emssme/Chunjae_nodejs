6aa7b233a7cb4861db74c5e4a14209da0597ad81
emsseme <bk75121@gmail.com>
2023-09-07 16:27:02 +0900

    서버만들기

--- js15.js
+++ js15.js
@@ -8,4 +8,4 @@ const server = http.createServer((req,res) => {
     res.writeHead(200, {'Content-Type' : 'text/html; charset=UTF-8'})
     res.end(content);
 });
-server.listen(3000);
+server.listen(3300);
